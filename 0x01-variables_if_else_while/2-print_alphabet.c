f2ab8b2ff2664061211edda7da78fe91e974b458
itskrisk <kriskamau25@gmail.com>
2022-10-18 04:54:44 -0700

    kris

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return alphasbets kris
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch = 'n';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
