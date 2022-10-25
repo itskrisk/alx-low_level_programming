af6068ec8145a404ee62a6fe6e71266838c17d65
itskrisk <kriskamau25@gmail.com>
2022-10-25 05:46:46 -0700

    kk

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - update pointer value to 98 kk
+ * @n: parameter to be provided
+ * Return: Always 0 (Successkris)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
